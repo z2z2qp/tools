d70b8586c8f6c546e1e518f482c598936dc896b3
庄佳彬 <zhuangjiabin@jrizju.com>
2023-11-22 10:05:00 +0800

    🐛：图片分析经纬度可用

--- src/main/java/cn/zjujri/workday/controller/ImageLocationController.kt
+++ src/main/java/cn/zjujri/workday/controller/ImageLocationController.kt
@@ -5,10 +5,12 @@ import cn.zjujri.workday.module.Result
 import cn.zjujri.workday.service.MetaDataService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
+import reactor.core.publisher.Mono
 
 @Tag(name = "图片")
 @RestController
@@ -18,12 +20,15 @@ class ImageLocationController(private val metaDataService: MetaDataService) {
 
     @Operation(summary = "获取经纬度")
     @PostMapping("/getLocation")
-    fun getLocation(file: MultipartFile): Result<GPS> {
-        val optional = metaDataService.parseLocation(file.inputStream)
-        val gps = optional.map {
-            GPS(it.longitude, it.latitude, null)
-        }.orElse(GPS())
-        return Result.ok(gps)
+    fun getLocation(@RequestPart("file") filePart: FilePart): Mono<Result<GPS>> {
+        return filePart.content()
+            .map {
+                val gps = metaDataService.parseLocation(it.asInputStream())
+                    .map { gl ->
+                        GPS(gl.longitude, gl.latitude, null)
+                    }.orElse(GPS())
+                Result.ok(gps)
+            }.next()
     }
 
 
